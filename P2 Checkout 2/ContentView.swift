cf9c6d9542453836375c5f4b88919fc49fcfe832
Dominik <dominik.ziegenhagel@gmail.com>
2023-10-29 11:35:41 +0100

    Initial Commit

---
+++ P2 Checkout 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P2 Checkout 2
+//
+//  Created by Eal on 29.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P2 Checkout 2/P2_Checkout_2App.swift
@@ -0,0 +1,17 @@
+//
+//  P2_Checkout_2App.swift
+//  P2 Checkout 2
+//
+//  Created by Eal on 29.10.23.
+//
+
+import SwiftUI
+
+@main
+struct P2_Checkout_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
